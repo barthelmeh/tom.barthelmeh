09e6f5d39c3ea635a1a5c20e0de51eded725ba99
Tom Barthelmeh <tom.barthelmeh@gmail.com>
2023-10-24 17:22:37 +1300

    Updated home page according to figma

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,5 +1,4 @@
 import NavBar from '../components/NavBar';
-import SkillsList from "../components/SkillsList";
 
 const HomePage = () => {
 
@@ -12,23 +11,23 @@ const HomePage = () => {
 
                         <div className="flex flex-col items-center justify-start w-full mb-8">
                             <div className="flex flex-col items-center justify-center mt-16 sm:mt-18 md:mt-24">
-                                <h1 className="font-display mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl">
+                                <h1 className="font-display mb-2 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-6xl lg:text-8xl">
                                     Tom Barthelmeh
                                 </h1>
-                                <p className="font-display text-xl font-bold text-blue-600">a creative portfolio</p>
+
+                                <p className="font-display tracking-tight text-gray-900">
+                                    Software Engineering student at UC
+                                </p>
+                            </div>
+
+                            <div className="flex flex-col items-center justify-center mt-12 sm:mt-14 md:mt-20">
+                                <a>
+                                    <button className="relative animate-slide-down rounded font-bold font-display [animation-delay:1.5s] before:absolute before:left-0 before:top-0 before:h-full before:w-full before:border-b before:border-text before:transition-all before:content-[''] hover:before:scale-x-100 active:before:scale-x-100 md:text-lg md:before:scale-x-0 md:before:border-b-2">
+                                        Download my CV
+                                    </button>
+                                </a>
                             </div>
                         </div>
-
-                        <SkillsList />
-
-                        <div className="flex flex-col items-center justify-center w-full">
-                            <div className="flex flex-col items-center justify-center pt-4 md:pt-6 w-[90%] mt-12 md:mt-16 rounded-lg md:flex-row bg-blue-600">
-
-                            </div>
-                        </div>
-
-
-
                     </div>
                 </div>
             </div>
