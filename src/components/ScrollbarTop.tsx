be1097761c0455d3a272e2224180a2338f6b2a54
Tom Barthelmeh <tom.barthelmeh@gmail.com>
2024-06-10 22:50:30 +1200

    Added horizontal scrollbar at the top of screen

--- src/components/ScrollbarTop.tsx
+++ src/components/ScrollbarTop.tsx
@@ -1,8 +1,24 @@
+import {motion, useScroll, useTransform} from "framer-motion";
 const ScrollbarTop = () => {
-    return (
-        <div>
 
-        </div>
+    const {scrollYProgress} = useScroll();
+
+    const background = useTransform(
+        scrollYProgress,
+        [0, 1],
+        ["#E3B6FF", "#61A0FF"]
+    );
+
+    return (
+        <motion.div
+            className='fixed left-1/2 top-0 z-20 w-screen h-4 bg-indigo-500 opacity-85'
+            style={{
+                scaleX: scrollYProgress,
+                background,
+                x: "-50%",
+                y: "-50%"
+            }}
+        />
     )
 }
 
