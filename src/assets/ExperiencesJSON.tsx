d15a5f937edc77e8fd5b9f2d09cb8fdfab8e31a3
Tom Barthelmeh <tom.barthelmeh@gmail.com>
2024-06-10 23:17:25 +1200

    Moved images so they are visible on deployment

--- src/assets/ExperiencesJSON.tsx
+++ src/assets/ExperiencesJSON.tsx
@@ -2,14 +2,14 @@ const ExperiencesJSON: Experience[] = [
     {
         title: 'Tutor',
         company: 'University of Canterbury',
-        company_image_url: '/src/assets/UC_logo.png',
+        company_image_url: 'UC_logo.png',
         from: 'Feb 2024',
         to: 'Present'
     },
     {
         title: 'Software Engineer Intern',
         company: 'Cavotec',
-        company_image_url: '/src/assets/cavotec_logo.png',
+        company_image_url: 'cavotec_logo.png',
         from: 'Nov 2022',
         to: 'Feb 2024'
     },
