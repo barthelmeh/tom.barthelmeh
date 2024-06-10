823ecafa1bc7240ee831f9c7f4241a2a7fa642aa
Tom Barthelmeh <tom.barthelmeh@gmail.com>
2024-06-10 17:04:18 +1200

    Removed home button from navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -4,12 +4,7 @@ const NavBar = () => {
 
     return (
         <div>
-            <nav className="w-full flex justify-between navbar text-xl font-accent text-black">
-                <div className="flex items-center justify-center">
-                    <a className="relative rounded [animation-delay:1.5s] before:absolute before:border-black before:left-0 before:top-0 before:h-full before:w-full before:border-b before:border-text before:transition-all before:content-[''] hover:before:scale-x-100 active:before:scale-x-100 md:before:scale-x-0 md:before:border-b-2">
-                        home
-                    </a>
-                </div>
+            <nav className="w-full flex justify-end navbar text-xl font-accent text-black">
                 <div className="flex space-x-6 justify-between">
                     <a href={LinkedInURL} target="_blank" className="relative rounded [animation-delay:1.5s] before:absolute before:border-black before:left-0 before:top-0 before:h-full before:w-full before:border-b before:border-text before:transition-all before:content-[''] hover:before:scale-x-100 active:before:scale-x-100 md:before:scale-x-0 md:before:border-b-2">
                         linkedin
