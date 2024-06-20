6364d3009e5a009a12bb4e46f4be42bbd6331cac
Tom Barthelmeh <tom.barthelmeh@gmail.com>
2024-06-20 18:42:54 +1200

    Added unique key

--- src/components/AnimatedCircles/AnimatedCircles.tsx
+++ src/components/AnimatedCircles/AnimatedCircles.tsx
@@ -2,10 +2,10 @@ import AnimatedCircle from "./AnimatedCircle.tsx";
 const AnimatedCircles = () => {
     return (
         <div className='flex absolute start-32 top-[30%] items-start space-x-0.5 bg-pink'>
-            <AnimatedCircle size={32} duration={7} key={1} delay={0.1}/>
+            <AnimatedCircle size={32}  duration={7} key={1} delay={0.1}/>
             <AnimatedCircle size={64} duration={9} key={3} delay={0} />
             <AnimatedCircle size={24} duration={7} key={2} delay={0.5}/>
-            <AnimatedCircle size={16} duration={6} key={1} delay={0.8}/>
+            <AnimatedCircle size={16} duration={6} key={4} delay={0.8}/>
         </div>
     )
 }
