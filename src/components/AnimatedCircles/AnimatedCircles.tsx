99c9d79ace3169770486adf3925470dc8914b8ee
Tom Barthelmeh <tom.barthelmeh@gmail.com>
2024-06-23 16:23:14 +1200

    Removed tailwind w and h classes as these were causing issues

--- src/components/AnimatedCircles/AnimatedCircles.tsx
+++ src/components/AnimatedCircles/AnimatedCircles.tsx
@@ -1,11 +1,11 @@
 import AnimatedCircle from "./AnimatedCircle.tsx";
 const AnimatedCircles = () => {
     return (
-        <div className='flex absolute start-32 top-[30%] items-start space-x-0.5 bg-pink'>
-            <AnimatedCircle size={32}  duration={7} key={1} delay={0.1}/>
-            <AnimatedCircle size={64} duration={9} key={3} delay={0} />
-            <AnimatedCircle size={24} duration={7} key={2} delay={0.5}/>
-            <AnimatedCircle size={16} duration={6} key={4} delay={0.8}/>
+        <div className='flex absolute start-32 top-[30%] items-start space-x-0.5'>
+            <AnimatedCircle size={128} duration={7} key={1} delay={10}/>
+            <AnimatedCircle size={256} duration={8} key={3} delay={0} />
+            <AnimatedCircle size={96} duration={6} key={2} delay={0.5}/>
+            <AnimatedCircle size={64} duration={5} key={4} delay={0.8}/>
         </div>
     )
 }
